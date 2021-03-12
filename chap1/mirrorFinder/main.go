bb004bddaff12c8d10253724441bcd8a142b6bb9
huytn1219 <huy.nguyen1219@gmail.com>
2021-03-12 15:49:55 -0800

    inital commit

--- chap1/mirrorFinder/main.go
+++ chap1/mirrorFinder/main.go
@@ -15,6 +15,25 @@ type response struct {
 	Latency    time.Duration `json:"latency"`
 }
 
+func findFastest(urls []string) response {
+	urlChan := make(chan string)
+	latencyChan := make(chan time.Duration)
+
+	for _, url := range urls {
+		mirrorURL := url
+		go func() {
+			start := time.Now()
+			_, err := http.Get(mirrorURL + "/README")
+			latency := time.Now().Sub(start) / time.Millisecond
+			if err == nil {
+				urlChan <- mirrorURL
+				latencyChan <- latency
+			}
+		}()
+	}
+	return response{<-urlChan, <-latencyChan}
+}
+
 func main() {
 	http.HandleFunc("/fastest-mirror", func(w http.ResponseWriter, r *http.Request) {
 		response := findFastest(mirrors.MirrorList)
